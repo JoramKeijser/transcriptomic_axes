c66e56a0805b323990a4fafc2027ea0997eab96e
Joram <joramkeijser@gmail.com>
2023-08-05 21:54:29 +0200

    rm unused imports

--- workflow/extract_tosches.R
+++ workflow/extract_tosches.R
@@ -1,15 +1,10 @@
 # Extract raw data from old Seurat object to something Python can handle
-library(dplyr)
-library(Seurat)
-#library(SeuratDisk)
 
 load(snakemake@input[["robj"]], verbose=TRUE)
 
 # Extract data
-counts <- attr(turtle.neurons, "raw.data") # genes x cells 
-meta.data <- attr(turtle.neurons, "data.info") # cells x features 
+counts <- attr(turtle.neurons, "raw.data") # genes x cells
+meta.data <- attr(turtle.neurons, "data.info") # cells x features
 
 write.table(meta.data, snakemake@output[["metadata"]])
 write.table(counts, snakemake@output[["counts"]])
-
-
