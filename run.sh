65bbe865a57dff30906988ce98c1b7bb2a897533
keijser <joramkeijser@gmail.com>
2024-04-27 09:32:25 +0200

    Run SLURM jobs on TestAndBuid partition

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=3850
-#SBATCH --time=01:00:00
+#SBATCH --time=4:00:00 #to allow queueing - 1h suffices for compute
 #SBATCH --output=log/main_%j
-#SBATCH --partition=standard
+#SBATCH --partition=TestAndBuild # alternative: standard
 
-snakemake --cores --profile env/slurm "$@" --cores 
+snakemake --profile env/slurm "$@" --cores 
