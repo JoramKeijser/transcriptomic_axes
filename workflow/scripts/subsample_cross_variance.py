e0214ecbd71bc06ab331663f7da5fbcba45b6d22
keijser <joramkeijser@gmail.com>
2024-05-07 22:28:13 +0200

    Cosmetic changes for L

--- workflow/scripts/subsample_cross_variance.py
+++ workflow/scripts/subsample_cross_variance.py
@@ -1,7 +1,6 @@
 """
 Mouse variance explained by other datasets
 """
-import pickle
 import numpy as np
 import anndata as ad
 import scanpy as sc
@@ -13,98 +12,102 @@ from src import constants
 
 sns.set_palette("Set2")
 sns.set_context("poster")
-sc.settings.figdir = "./figures/figure2/"
 sc.settings.dpi_save = 300
 
 
-def main():
-    plt.figure()
-    colors = {
-            "tasic": sns.color_palette('Set2')[0],
-            "bakken": sns.color_palette("Set2")[1],
-            "colquitt": sns.color_palette("Set2")[2],
-            "tosches": sns.color_palette("Set2")[3],
-        }
-    species = {
-        "colquitt": "Zebra finch",
-        "tosches": "Turtle",
-        "tasic": "Mouse",
-        "bakken": "Human",
-    }
-    reference = snakemake.params.reference
-    datasets = {}
-    seeds = []
-    print("Loading data")
-    for dataset in snakemake.input:
-        name = dataset.split("/")[-1].split("h5ad")[0].split("_")[0]
-        seed = int(dataset.split("/")[-1].split(".h5ad")[0].split("_")[-1])
-        if name == snakemake.params.reference:
-            seeds.append(seed)
-        if name not in datasets.keys():
-            datasets[name] = []
-        datasets[name].append(ad.read_h5ad(dataset))
+colors = {
+    "tasic": sns.color_palette("Set2")[0],
+    "bakken": sns.color_palette("Set2")[1],
+    "colquitt": sns.color_palette("Set2")[2],
+    "tosches": sns.color_palette("Set2")[3],
+}
+species = {
+    "colquitt": "Zebra finch",
+    "tosches": "Turtle",
+    "tasic": "Mouse",
+    "bakken": "Human",
+}
+# Load data
+reference = snakemake.params.reference
+datasets = {}
+seeds = []
+print("Loading data")
+for dataset in snakemake.input:
+    name = dataset.split("/")[-1].split("h5ad")[0].split("_")[0]
+    seed = int(dataset.split("/")[-1].split(".h5ad")[0].split("_")[-1])
+    if name == snakemake.params.reference:
+        seeds.append(seed)
+    if name not in datasets.keys():
+        datasets[name] = {}
+    datasets[name][seed] = ad.read_h5ad(dataset)
 
+print(seeds)
+cross_variance = {}  # dist of lists, one for each dataset
+variance = []  # one for each seed
+print("Compute cross-covariance")
+for seed in seeds:
     # PCA on reference
-    for seed in seeds:
-        cross_variance = {}
-        hvgs = datasets[reference][seed].var.highly_variable
-        datasets[reference][seed] = datasets[reference][seed][:, hvgs]
-        pca = PCA(n_components=constants.NUM_PCS).fit(datasets[reference][seed].X)
-        C = pca.get_covariance()
-        w = pca.components_[0]
-        variance = w @ C @ w / np.trace(C)
+    hvgs = datasets[reference][seed].var.highly_variable
+    datasets[reference][seed] = datasets[reference][seed][:, hvgs]
+    pca = PCA(n_components=constants.NUM_PCS).fit(datasets[reference][seed].X)
+    C = pca.get_covariance()
+    w = pca.components_[0]
+    # Normalized variance for reference
+    unnormed_var = np.diag(
+        pca.components_[:10] @ C @ pca.components_[:10].T
+    ) / np.trace(C)
+    variance.append(unnormed_var / unnormed_var[0])
 
-        # Another pass
-        print("Compute cross-covariance")
-        seed_datasets = [key for key in datasets.keys()]
-        for name in seed_datasets:
-            dataset = datasets[name][seed]
-            if name == reference:
-                continue
-            # Focus on HVGs of reference
-            datasets[name][seed] = datasets[name][seed][:, hvgs]
-            cross_variance[name] = compare_variance(
-                datasets[reference][seed], datasets[name][seed], pca
-            )
-
-        variance = np.diag(pca.components_[:10] @ C @ pca.components_[:10].T) / np.trace(C)
-        variance /= variance[0]
-
-        # Plot
-        x = np.arange(5) * 4
-        # Chance level: random components
-        rng = np.random.RandomState(0)
-        random_components = rng.normal(size=(10, C.shape[0]))
-        random_components /= np.linalg.norm(random_components, axis=1, keepdims=True)
-        random_variance = np.diag(
-            random_components[:10] @ C @ random_components[:10].T
-        ) / float(pca.components_[0] @ C @ pca.components_[0])
-        print(f"random tPC1: {random_variance[0] * 100:0.1f}% of mouse tPC1")
-        print(f"random tPC1-10: {np.sum(random_variance) * 100:0.1f}% of mouse tPC1")
-        plt.hlines(
-            random_variance[0], -1, 20, color="black", linestyles=":", lw=3,
+    # Another pass, now across other datasets
+    seed_datasets = [key for key in datasets.keys()]
+    for name in seed_datasets:
+        dataset = datasets[name][seed]
+        if name == reference:
+            continue
+        # Focus on HVGs of reference
+        datasets[name][seed] = datasets[name][seed][:, hvgs]
+        if name not in cross_variance.keys():
+            cross_variance[name] = []
+        cross_variance[name].append(
+            compare_variance(datasets[reference][seed], datasets[name][seed], pca)
         )
-        print(f"tasic tPC1-10: {variance[:10].sum()*100:0.1f}% of mouse tPC1")
+
+# Nor plt them all
+x = np.arange(5) * 4
+plt.scatter(
+    x, np.array(variance)[:, :5].mean(0), color=colors[reference], marker="d", s=100
+)
+for i_s, seed in enumerate(seeds):
+    plt.scatter(
+        x, np.array(variance)[:, :5].mean(0), color=colors[reference], s=20, alpha=0.2
+    )
+
+
+for i, name in enumerate(["bakken", "colquitt", "tosches"]):
+    plt.scatter(
+        x + 0.8 * (i + 1),
+        np.array(cross_variance[name])[:, :5].mean(0),
+        color=colors[name],
+        marker="d",
+        s=100,
+    )
+
+    for i_s, seed in enumerate(seeds):
         jitter = np.random.uniform(-0.3, 0.3)
-        plt.scatter(x + jitter, variance[:5], s=30, alpha=0.3, color=colors[reference])
+        plt.scatter(
+            x + 0.8 * (i + 1) + jitter,
+            np.array(cross_variance[name])[i_s, :5],
+            color=colors[name],
+            s=20,
+            alpha=0.2,
+        )
 
-        for i, name in enumerate(["bakken", "colquitt", "tosches"]):
-            jitter = np.random.uniform(-0.3, 0.3)
-            plt.scatter(x + 0.8 * (i + 1) + jitter, cross_variance[name][:5], s=30, alpha=0.3,
-                        color=colors[name])
-            print(
-                f"{name} tPC1-10: {cross_variance[name][:10].sum()*100:0.1f}% of mouse tPC1"
-            )
-    plt.xticks(x + 1.0, np.arange(5) + 1)
-    plt.yticks([0, 0.5, 1])
-    sns.despine()
-    plt.ylabel("Variance (norm.)")
-    plt.xlabel("tPC")
-    plt.ylim([-0.04, 1.1])
-    plt.tight_layout()
-    # Save figure
-    plt.savefig(snakemake.output.figure, dpi=300)
-
-
-if __name__ == "__main__":
-    main()
+plt.xticks(x + 1.0, np.arange(5) + 1)
+plt.yticks([0, 0.5, 1])
+sns.despine()
+plt.ylabel("Variance (norm.)")
+plt.xlabel("tPC")
+plt.ylim([-0.04, 1.1])
+plt.tight_layout()
+# Save figure
+plt.savefig(snakemake.output.figure, dpi=300)
--- workflow/scripts/subsample_principal_angles.py
+++ workflow/scripts/subsample_principal_angles.py
@@ -1,7 +1,6 @@
 """
 Compute and visualize principal angles
 """
-import pickle
 import numpy as np
 import anndata as ad
 import scanpy as sc
@@ -10,19 +9,21 @@ import seaborn as sns
 from scipy.linalg import subspace_angles
 from src import constants
 
+# Settings for plotting
 sns.set_context("poster")
 sc.settings.dpi_save = 300
+MARKERSIZE = 20
+ALPHA = 0.2
+LINEWIDTH = 3
 
 print("Loading PCs")
 PC_subspace = {}
 seeds = []
 for dataset in snakemake.input:
-    print(dataset)
     name = dataset.split("/")[-1].split("h5ad")[0].split("_")[0]
     seed = int(dataset.split("/")[-1].split(".h5ad")[0].split("_")[-1])
     if name == snakemake.params.reference:
         seeds.append(seed)
-    print(dataset, name, seed)
     adata = ad.read_h5ad(dataset)
     if name not in PC_subspace.keys():
         PC_subspace[name] = []
@@ -30,7 +31,6 @@ for dataset in snakemake.input:
 
 for name in PC_subspace.keys():
     PC_subspace[name] = np.array(PC_subspace[name])
-    print(PC_subspace[name].shape)
 
 print("Plotting principal angles")
 reference = snakemake.params.reference
@@ -39,20 +39,20 @@ remaining = np.sort([dataset for dataset in PC_subspace.keys() if dataset != ref
 
 # Add random
 n_genes = PC_subspace[reference][0].shape[0]
+n_pcs = PC_subspace[reference][seeds[0]].shape[1]
 rng = np.random.RandomState(0)
-for seed in seeds:
+angles = np.zeros((len(seeds), n_pcs))
+for i_s, seed in enumerate(seeds):
     idx = rng.permutation(np.arange(n_genes))
-    angles = (
+    angles[i_s] = (
         subspace_angles(
             PC_subspace[reference][seed][idx], PC_subspace[reference][seed]
         )[::-1]
         * 180
         / np.pi
     )
-    if seed == min(seeds):  # add label
-        plt.plot(angles, label="Chance", color="black", linestyle=":", lw=1, alpha=0.5)
-    else:
-        plt.plot(angles, color="black", linestyle=":", lw=1, alpha=0.5)
+    plt.scatter(np.arange(n_pcs), angles[i_s], color="gray", s=MARKERSIZE, alpha=ALPHA)
+plt.plot(np.arange(n_pcs), angles.mean(0), label="Chance", color="gray", lw=LINEWIDTH)
 
 colors = {
     "bakken": sns.color_palette("Set2")[1],
@@ -61,30 +61,34 @@ colors = {
 }
 # Compare reference
 print("Seeds: ", seeds)
-for seed in seeds:
-    for i, name in enumerate(remaining):
-        angles = (
+for i, name in enumerate(remaining):
+    angles = np.zeros((len(seeds), n_pcs))
+    for i_s, seed in enumerate(seeds):
+        angles[i_s] = (
             subspace_angles(PC_subspace[name][seed], PC_subspace[reference][seed])[::-1]
             * 180
             / np.pi
         )
-        print(f"Seed {seed}: {angles[0]:0.2f}")
-        if seed == min(seeds):  # Label once
-            plt.plot(
-                angles,
-                label=snakemake.params.areas[name],
-                color=colors[name],
-                lw=1,
-                alpha=0.5,
-            )
-        else:
-            print("plot seed")
-            plt.plot(angles, color=colors[name], lw=1, alpha=0.5)
+        plt.scatter(
+            np.arange(n_pcs),
+            angles[i_s],
+            color=colors[name],
+            alpha=ALPHA,
+            s=MARKERSIZE,
+        )
+    # Plot mean
+    plt.plot(
+        np.arange(n_pcs),
+        angles.mean(0),
+        label=snakemake.params.areas[name],
+        color=colors[name],
+        lw=LINEWIDTH,
+    )
 
 plt.xlabel("tPC subspace dimension")
 plt.ylabel("Principal angle (deg.)")
 plt.yticks([50, 70, 90])
-plt.legend(handlelength=0.5)
+plt.legend(handlelength=0.5, framealpha=0.2)
 sns.despine()
 plt.tight_layout()
 plt.savefig(snakemake.output.figure, dpi=300)
